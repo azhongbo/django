36e0df7cff2a357744c4d25f6bfe7e2cf7f0c137
azhongbo <azhongbo@gmail.com>
2024-01-11 18:00:03 +0800

    c3 static test

--- web/main/settings.py
+++ web/main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-n_1p%61*++azydkj4*$kmkf$n&+p@^%f21i^qju@kx%p3cp=gi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
